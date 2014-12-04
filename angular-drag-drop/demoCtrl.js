a18ac42623471f52cac4b479e2015e4bfbcc3c90
L. Gabriel Miller <gmiller2007@gmail.com>
2014-12-03 19:39:00 -0500

    Adds ability to add and remove bucketables. Makes initial variable values set by functions rather than literals.

--- angular-drag-drop/demoCtrl.js
+++ angular-drag-drop/demoCtrl.js
@@ -7,96 +7,117 @@
     function demoCtrl() {
         var that = this;
 
-        var bucketables = [
-            {
-                category: 1,
-                id: 1,
-                label: "One"
-            },
-            {
-                category: 2,
-                id: 2,
-                label: "Zwei"
-            },
-            {
-                category: 2,
-                id: 3,
-                label: "Tres"
-            },
-            {
-                category: 3,
-                id: 4,
-                label: "Quatre"
-            },
-            {
-                category: 3,
-                id: 5,
-                label: "Khamsa"
+        // Methods
+        that.addBucketable = addBucketable;
+        that.deleteBucketable = deleteBucketable;
+        that.dump = dump;
+        that.swapElements = swapElements;
+
+        // Variables
+        that.bucketables = getBucketables();
+        that.newBucketable = getNewBucketable();
+        that.sortables = getSortables();
+
+        function addBucketable(label) {
+
+            if (label === "") {
+                return;
             }
-        ];
 
-        var newBucketable = {
-            name: ""
-        };
+            var newId;
 
-        var sortables = [
-            {
-                id: 1,
-                label: "Hamburger",
-                number: 1
-            },
-            {
-                id: 2,
-                label: "Bun",
-                number: 2
-            },
-            {
-                id: 3,
-                label: "Cheese",
-                number: 3
-            },
-            {
-                id: 4,
-                label: "Ketchup",
-                number: 4
-            },
-            {
-                id: 5,
-                label: "Lettuce",
-                number: 5
+            if (that.bucketables.length === 0) {
+                newId = 1;
+            } else {
+                var lastIndex = that.bucketables.length - 1;
+                newId = that.bucketables[lastIndex].id + 1;
             }
-        ];
 
-        function addBucketable(name) {
-            console.log("adding bucketable", name);
-            var newBucketableIndex = that.bucketables.length + 1;
-            console.log(newBucketableIndex);
-            var newItem = {
+            that.bucketables.push({
                 category: 0,
-                id: newBucketableIndex,
-                label: name
-            };
-            console.log(newItem);
-            //that.bucketables.push(newItem);
+                id: newId,
+                label: label
+            });
+            that.newBucketable = getNewBucketable();
+        }
+
+        function deleteBucketable(index) {
+            that.bucketables.splice(index, 1);
         }
 
         function dump() {
             console.log(that);
         }
 
+        function getBucketables() {
+            return [
+                {
+                    category: 1,
+                    id: 1,
+                    label: "One"
+                },
+                {
+                    category: 2,
+                    id: 2,
+                    label: "Zwei"
+                },
+                {
+                    category: 2,
+                    id: 3,
+                    label: "Tres"
+                },
+                {
+                    category: 3,
+                    id: 4,
+                    label: "Quatre"
+                },
+                {
+                    category: 3,
+                    id: 5,
+                    label: "Khamsa"
+                }
+            ];
+        }
+
+        function getNewBucketable() {
+            return {
+                label: ""
+            };
+        }
+
+        function getSortables() {
+            return [
+                {
+                    id: 1,
+                    label: "Hamburger",
+                    number: 1
+                },
+                {
+                    id: 2,
+                    label: "Bun",
+                    number: 2
+                },
+                {
+                    id: 3,
+                    label: "Cheese",
+                    number: 3
+                },
+                {
+                    id: 4,
+                    label: "Ketchup",
+                    number: 4
+                },
+                {
+                    id: 5,
+                    label: "Lettuce",
+                    number: 5
+                }
+            ];
+        };
+
         function swapElements() {
             console.log("Swapping Elements");
         }
-
-        // Methods
-        that.addBucketable = addBucketable;
-        that.dump = dump;
-        that.swapElements = swapElements;
-
-        // Variables
-        that.bucketables = bucketables;
-        that.newBucketable = newBucketable;
-        that.sortables = sortables;
     }
 
 })();
